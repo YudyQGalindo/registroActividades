50b85ff145e786e2107bee5a7711495538c364b5
YudyQGalindo <yudiquintero.9@gmail.com>
2023-10-09 14:43:35 -0500

    Registro de usuarios

--- tasks/views.py
+++ tasks/views.py
@@ -2,57 +2,61 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
-from django.http import HttpResponse
+from django.http import HttpResponseForbidden
 from .forms import createTaskForm
 from .models import Task
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+# Mostrar la página principal
 def home(request):
     return render(request, 'home.html')
 
+# Registrar un nuevo usuario
 def signup(request):
-
     if request.method == 'GET':
         return render(request, 'signup.html', {
             'form': UserCreationForm
         })
-
     else:
+        # Verifica si el usuario actual es Admin
+        if request.user.username == 'Admin':
+            if request.POST['password1'] == request.POST['password2']:
+                # Registro de usuario
+                try:
+                    user = User.objects.create_user(
+                        username=request.POST['username'], password=request.POST['password1'])
+                    user.save()
+                    login(request, user)
+                    return redirect('tasks')
+                except:
+                    return render(request, 'signup.html', {
+                        'form': UserCreationForm,
+                        "error": 'Username already exists'
+                    })
+            return render(request, 'signup.html', {
+                'form': UserCreationForm,
+                "error": 'Password do not match'
+            })
+        else:
+            # Si el usuario no es Admin, mostrar una página de acceso denegado
+            return HttpResponseForbidden("Acceso denegado")
 
-        if request.POST['password1'] == request.POST['password2']:
-            # register user
-
-            try:
-                user = User.objects.create_user(
-                    username=request.POST['username'], password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('tasks')
-
-            except:
-                return render(request, 'signup.html', {
-                    'form': UserCreationForm,
-                    "error": 'Username already exists'
-                })
-
-        return render(request, 'signup.html', {
-            'form': UserCreationForm,
-            "error": 'Password do not match'
-        })
-
+# Mostrar las actividades pendientes de un usuario
 @login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {'tasks': tasks})
 
+# Mostrar las actividades completadas de un usuario
 @login_required
 def tasks_completed(request):
-    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    tasks = Task.objects.filter(
+        user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'tasks.html', {'tasks': tasks})
 
+# Crear una nueva actividad
 @login_required
 def create_task(request):
     if request.method == 'GET':
@@ -68,10 +72,11 @@ def create_task(request):
             return redirect('tasks')
         except ValueError:
             return render(request, 'create_task.html', {
-            'form': createTaskForm,
-            'error': 'Please provide valida data'
+                'form': createTaskForm,
+                'error': '¡Error: por favor ingrese datos válidos!'
             })
 
+# Actualizar las actividades pendientes de un usuario
 @login_required
 def task_detail(request, task_id):
     if request.method == 'GET':
@@ -88,10 +93,11 @@ def task_detail(request, task_id):
             return redirect('tasks')
         except ValueError:
             return render(request, 'task_detail.html', {
-            'task': task, 'form': form,
-            'error': "Error updating task"
+                'task': task, 'form': form,
+                'error': "Error: al actualizar los datos, intente de nuevo."
             })
 
+# Marcar una actividad como completada
 @login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
@@ -100,6 +106,7 @@ def complete_task(request, task_id):
         task.save()
         return redirect('tasks')
 
+# Eliminar una actividad
 @login_required
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
@@ -107,11 +114,13 @@ def delete_task(request, task_id):
         task.delete()
         return redirect('tasks')
 
+# Cerrar sesión
 @login_required
 def signout(request):
     logout(request)
     return redirect('home')
 
+# Inicio de sesión
 def signin(request):
     if request.method == 'GET':
         return render(request, 'signin.html', {
@@ -120,11 +129,18 @@ def signin(request):
     else:
         user = authenticate(
             request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:
-            return render(request, 'signin.html', {
-                'form': AuthenticationForm,
-                'error': 'Username or password is incorrect!'
-            })
+        if user is not None:
+            if user.username == 'Admin':
+                # El usuario es un administrador, redirigir a la vista de registro de usuarios
+                login(request, user)
+                return redirect('signup')
+            else:
+                # El usuario no es un administrador, redirigir a la vista de tareas pendientes
+                login(request, user)
+                return redirect('tasks')
         else:
-            login(request, user)
-            return redirect('tasks')
+            # Los datos ingresados están erróneos
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm(),
+                'error': '¡Error: Usuario o contraseña incorrectos!'
+            })
